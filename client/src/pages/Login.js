8bc6ee9ea45c6a436836f65453014fa0c10f20fc
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-10-14 17:49:50 -0400

    updated font on login and singup page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error }] = useMutation(LOGIN);
 
   const handleFormSubmit = async (event) => {
@@ -31,12 +31,30 @@ function Login(props) {
 
   return (
     <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
+      <Link to="/signup">
+        <img
+          src="https://fontmeme.com/permalink/211014/d2d4b17be0788711f5856aec8abb4e51.png"
+          alt="pokemon-font"
+          border="0"
+        ></img>
+      </Link>
 
-      <h2>Login</h2>
+      <h2>
+        <img
+          src="https://fontmeme.com/permalink/211014/9ac5e3a479690177435a1c2af2e27d81.png"
+          alt="pokemon-font"
+          border="0"
+        ></img>
+      </h2>
       <form onSubmit={handleFormSubmit}>
         <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
+          <label htmlFor="email">
+            <img
+              src="https://fontmeme.com/permalink/211014/3795834efcb30e69c219dc8763a08aaf.png"
+              alt="pokemon-font"
+              border="0"
+            ></img>
+          </label>
           <input
             placeholder="youremail@test.com"
             name="email"
@@ -46,7 +64,13 @@ function Login(props) {
           />
         </div>
         <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+          <label htmlFor="pwd">
+            <img
+              src="https://fontmeme.com/permalink/211014/2ea275d84e597be32dff58e8142d93f4.png"
+              alt="pokemon-font"
+              border="0"
+            ></img>
+          </label>
           <input
             placeholder="******"
             name="password"
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
 
 function Signup(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
@@ -31,12 +31,30 @@ function Signup(props) {
 
   return (
     <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+      <Link to="/login">
+        <img
+          src="https://fontmeme.com/permalink/211014/471fe6761cb14bcc3c8e464afdab4d68.png"
+          alt="pokemon-font"
+          border="0"
+        ></img>
+      </Link>
 
-      <h2>Signup</h2>
+      <h2>
+        <img
+          src="https://fontmeme.com/permalink/211014/3c2b3c44c030afc634018bc3993f60ae.png"
+          alt="pokemon-font"
+          border="0"
+        ></img>
+      </h2>
       <form onSubmit={handleFormSubmit}>
         <div className="flex-row space-between my-2">
-          <label htmlFor="username">Username:</label>
+          <label htmlFor="username">
+            <img
+              src="https://fontmeme.com/permalink/211014/1afb81a824d8a8dccf7b2f78afa6f15c.png"
+              alt="pokemon-font"
+              border="0"
+            ></img>
+          </label>
           <input
             placeholder="Username"
             name="username"
@@ -45,9 +63,16 @@ function Signup(props) {
             onChange={handleChange}
           />
         </div>
-        
+
         <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email">
+            {" "}
+            <img
+              src="https://fontmeme.com/permalink/211014/3795834efcb30e69c219dc8763a08aaf.png"
+              alt="pokemon-font"
+              border="0"
+            ></img>
+          </label>
           <input
             placeholder="youremail@test.com"
             name="email"
@@ -57,7 +82,13 @@ function Signup(props) {
           />
         </div>
         <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
+          <label htmlFor="pwd">
+            <img
+              src="https://fontmeme.com/permalink/211014/2ea275d84e597be32dff58e8142d93f4.png"
+              alt="pokemon-font"
+              border="0"
+            ></img>
+          </label>
           <input
             placeholder="******"
             name="password"
